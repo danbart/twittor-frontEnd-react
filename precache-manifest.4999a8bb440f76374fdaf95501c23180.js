758a66e9adb5cda7bc955080aec253e91b43f874
danbart <danilo_solorzano06@hotmail.com>
2020-05-30 21:57:29 -0600

    Updates

---
+++ precache-manifest.4999a8bb440f76374fdaf95501c23180.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dbfcb39ee03026e553ba03c4bbf172",
+    "url": "/twittor-frontEnd-react/index.html"
+  },
+  {
+    "revision": "88fe6f8df238221b8b14",
+    "url": "/twittor-frontEnd-react/static/css/2.cd817f6e.chunk.css"
+  },
+  {
+    "revision": "db6daa41a6d88042d8ab",
+    "url": "/twittor-frontEnd-react/static/css/main.fca3c0f5.chunk.css"
+  },
+  {
+    "revision": "88fe6f8df238221b8b14",
+    "url": "/twittor-frontEnd-react/static/js/2.6256d803.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/twittor-frontEnd-react/static/js/2.6256d803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6daa41a6d88042d8ab",
+    "url": "/twittor-frontEnd-react/static/js/main.dff37d01.chunk.js"
+  },
+  {
+    "revision": "3e8a0855ab7ecb26333c",
+    "url": "/twittor-frontEnd-react/static/js/runtime-main.89c31c9a.js"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/twittor-frontEnd-react/static/media/camara.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/twittor-frontEnd-react/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/twittor-frontEnd-react/static/media/date-birth.9b45fe49.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/twittor-frontEnd-react/static/media/error-404.d3c57e3b.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/twittor-frontEnd-react/static/media/link.a108520e.svg"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/twittor-frontEnd-react/static/media/location.02f2070b.svg"
+  },
+  {
+    "revision": "41f65f75cb0b41db6fe44ab4b074bacc",
+    "url": "/twittor-frontEnd-react/static/media/logo-white.41f65f75.png"
+  },
+  {
+    "revision": "1769587d7e1330e9d4241c30f672c7ff",
+    "url": "/twittor-frontEnd-react/static/media/logo.1769587d.png"
+  }
+]);
